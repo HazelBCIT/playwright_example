27f1134fc91217674170d3ff418eb9301be8c9a8
HazelBCIT <ywang600@my.bcit.ca>
2023-01-24 20:20:09 -0800

    set up

---
+++ pages/about.js
@@ -0,0 +1,47 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function About() {
+  return (
+  <>
+   <Head>
+        <title>About Us</title>
+        <meta name="author" content="MDIA 2109" />
+        <meta property="og:title" content="Assignment #1 - About Us Page" />
+        <meta property="og:description" content="BCIT Digital Design and Development Diploma" />
+        <link rel="icon" href="/favicon.png" />
+  </Head>
+
+    <main className={styles.main}>
+      <div>
+        <h1></h1>
+      </div>
+
+      <div>
+        <h1></h1>
+      </div>
+
+      <div>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+      </div>
+        
+      <div>
+        <h2></h2>
+        <h2></h2>
+      </div>
+
+      <div>
+        <h3></h3>
+      </div>
+      
+    </main>
+    </>
+  )
+  
+}
